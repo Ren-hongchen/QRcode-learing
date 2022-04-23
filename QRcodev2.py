652d693769d95d2a8b98b5467a7944ee2865ff7d
Ren-hongchen <1280656287@qq.com>
2022-04-23 20:47:22 +0800

    polynomial

--- QRcodev2.py
+++ QRcodev2.py
@@ -1,3 +1,4 @@
+from signal import raise_signal
 from constant import *
 import re
 import utils
@@ -6,7 +7,7 @@ class QRcode:
     __str = ""
     __strLength = 0
     __encodeMode = 1
-    __ECLevel = 1 # Error Correction Level 0 L 1 M 2 Q 3 H
+    __ECLevel = 1 # Error Correction Level 0-L 1-M 2-Q 3-H
     __version = 1 # QRcode size
     __data = "" # encoded data
 
@@ -28,13 +29,13 @@ class QRcode:
     def __setEncodeMode(self) -> None:  
         if(self.__str.isdigit()):
             self.__encodeMode = 0
+            return
 
-        AlphanumericModePattern = re.compile(b"^["+ re.escape(AlphanumericModeStr) + rb"]*\Z")
-        middleStr = self.__str   # avoid modifying self.__str 
-        if(AlphanumericModePattern.match(middleStr.encode("ISO 8859-1"))):
+        AlphanumericModePattern = re.compile(b"^["+ re.escape(AlphanumericModeStr) + rb"]*\Z") 
+        if(AlphanumericModePattern.match(self.__str.encode("ISO 8859-1"))):
             self.__encodeMode = 1
-        
-        self.__encodeMode = 2
+        else:
+            self.__encodeMode = 2
 
     # 1-40 versions
     def __setVersion(self) -> int:
@@ -66,20 +67,23 @@ class QRcode:
 
     def __setCCIndicator(self) -> None: #set character count indicator
         if(self.__version <= 9):
-            if(self.__encodeMode == 0): self.setIndicator(10)
-            if(self.__encodeMode == 1): self.setIndicator(9)
-            if(self.__encodeMode == 2): self.setIndicator(8)
-            if(self.__encodeMode == 3): self.setIndicator(8)
+            if(self.__encodeMode == 0): self.__setIndicator(10)
+            elif(self.__encodeMode == 1): self.__setIndicator(9)  
+            elif(self.__encodeMode == 2): self.__setIndicator(8)  
+            elif(self.__encodeMode == 3): self.__setIndicator(8)
+            return
         if(self.__version <= 26):
-            if(self.__encodeMode == 0): self.setIndicator(12)
-            if(self.__encodeMode == 1): self.setIndicator(11)
-            if(self.__encodeMode == 2): self.setIndicator(16)
-            if(self.__encodeMode == 3): self.setIndicator(10)
+            if(self.__encodeMode == 0): self.__setIndicator(12)
+            elif(self.__encodeMode == 1): self.__setIndicator(11)
+            elif(self.__encodeMode == 2): self.__setIndicator(16)
+            elif(self.__encodeMode == 3): self.__setIndicator(10)
+            return
         if(self.__version <= 40):
-            if(self.__encodeMode == 0): self.setIndicator(14)
-            if(self.__encodeMode == 1): self.setIndicator(13)
-            if(self.__encodeMode == 2): self.setIndicator(16)
-            if(self.__encodeMode == 3): self.setIndicator(12)
+            if(self.__encodeMode == 0): self.__setIndicator(14)
+            elif(self.__encodeMode == 1): self.__setIndicator(13)
+            elif(self.__encodeMode == 2): self.__setIndicator(16)
+            elif(self.__encodeMode == 3): self.__setIndicator(12)
+            return
 
     def __encodeInputText(self):
         if(self.__encodeMode == 0):
@@ -93,8 +97,7 @@ class QRcode:
         # else:
         #     self.KanjiModeEncoding()
 
-    def __NumericModeEncoding(self) -> list:
-        data = []
+    def __NumericModeEncoding(self):
         strlist = re.findall(r'/d{3}', self.__str)
         if(self.__strLength % 3 == 1):
             strlist.append(self.__str[-1])
@@ -102,29 +105,51 @@ class QRcode:
             strlist.append(self.__str[-2:])
         for i in strlist:
             if(i[0] == 0 and i[1] == 0):
-                data.append(bin(int(i))[2:].zfill(4))
+                self.__data += bin(int(i))[2:].zfill(4)
             elif(i[0] == 0):
-                data.append(bin(int(i))[2:].zfill(7))
+                self.__data += bin(int(i))[2:].zfill(7)
             else:
-                data.append(bin(int(i))[2:].zfill(10))
-        return data
+                self.__data += bin(int(i))[2:].zfill(10)
+        return
 
-    def __AlphanumericModeEncoding(self) -> list:
-        data = []
+    def __AlphanumericModeEncoding(self):
         strlist = re.findall(r'.{2}', self.__str)
         for i in strlist:
-                data.append(
-                    bin(AlphanumericModeMap[i[0]]*45 + AlphanumericModeMap[i[1]])[2:].zfill(11)
-                )
+            self.__data += bin(AlphanumericModeMap[i[0]]*45 + AlphanumericModeMap[i[1]])[2:].zfill(11)
         if(self.__strLength % 2 == 1):
-            data.append(bin(AlphanumericModeMap[self.__str[-1]]).zfill(6))
-        return data
+            self.__data += bin(AlphanumericModeMap[self.__str[-1]])[2:].zfill(6)
+        return
 
-    def __ByteModeEncoding(self) -> list:
-        data = []
-        self.__str.encode(encoding='ISO 8859-1')
-        for i in self.__str:
-            data.append(bin(i)[2:].zfill(8))
+    def __ByteModeEncoding(self):
+        for i in self.__str.encode(encoding="ISO 8859-1"):
+            self.__data += bin(i)[2:].zfill(8)
+
+    def __addPadBytestoData(self):
+        # add terminator if necessary
+        maxCapacity = utils.getMaxCapacity(self.__version, self.__ECLevel)
+        n = maxCapacity - len(self.__data)
+        if(n < 0):
+            raise Exception("data > maxCapacity")
+        elif(n >= 4):
+            for i in range(0,4): self.__data += '0'
+        else:
+            for i in range(0,n): self.__data += '0'
+        # Add More 0s to Make the Length a Multiple of 8
+        for i in range(0,(8 - (len(self.__data) % 8))):
+            self.__data += '0'
+        # Add Pad Bytes if the String is Still too Short
+        flag = True
+        while((maxCapacity - len(self.__data)) > 0):
+            if(flag): self.__data += '11101100'
+            else: self.__data += '00010001'
+            flag = not flag
+
+        return
+
+    def __setErrorCorrectCodes(self):
+        messagePolynomial = utils.getMessagePolynomial(self.__data)
+        generatorPolynomial = utils.getGeneratorPolynomial(self.__version,self.__ECLevel)
+        return
 
     def make(self):
         self.__setEncodeMode()
@@ -132,6 +157,11 @@ class QRcode:
             self.__setVersion()
         self.__checkVersion()
         self.__setModeIndicator()
+        self.__setCCIndicator()
+        self.__encodeInputText()
+        self.__addPadBytestoData()
+        self.__setErrorCorrectCodes()
+
         
 
     
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 from constant import *
+import math
+
 
 # CapacityTable is not strictly orderly tuple, 
 # but we can noticed that tuple is orderly when we have fixed level and encodeMode,
@@ -15,3 +17,58 @@ def bisect(encodeMode,level,target):
         else:
             high = mid
     return low + 1
+
+def getMaxCapacity(version,level) -> int:
+    return BlockTable[(version - 1) * 4 + level][0] * 8
+
+def getGeneratorPolynomial(version,level):
+    ECcodewords = BlockTable[(version - 1) * 4 + level][1]
+    return caculateGeneratorPolynomial(ECcodewords)
+
+def caculateGeneratorPolynomial(ECcodewords):
+    if(ECcodewords == 1):
+        return [0,0]
+    return mul(caculateGeneratorPolynomial(ECcodewords - 1), [0,ECcodewords - 1])
+
+def mul(a,b):
+    result = [None] * (b[1] + 2)
+    for i,item1 in enumerate(a):
+        for j, item2 in enumerate(b):
+            if(result[i+j] == None):
+                result[i+j] = item1 + item2
+            else:
+                AlphaNumber = getAlpha(result[i+j]) ^ getAlpha(item1 + item2)
+                result[i+j] = getAlphaIndex(AlphaNumber)
+    return result
+
+
+def getAlpha(a):
+    if(a == 0):
+        return 1
+    result = getAlpha(a-1) * 2
+    while(result > 255):
+        result = result ^ 285
+    return result
+
+def getAlphaIndex(a):
+    if(math.log(a,2) % 1 == 0):
+        return int(math.log(a,2))
+    elif(a % 2 == 0):
+        return getAlphaIndex(a // 2) + 1
+    else:
+        a = a ^ 285
+        return getAlphaIndex(a)
+
+
+def getMessagePolynomial(data):
+    messagePolynomial = []
+    for i in range(0,len(data),8):
+        a = int(data[i:i+8],2)
+        messagePolynomial.append(a)
+    return messagePolynomial
+
+
+
+
+    
+    
